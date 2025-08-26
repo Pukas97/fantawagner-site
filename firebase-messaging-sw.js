286f269b1448d172273eafedd9942eb1ad470133
Pukas97 <lorenzo.panzeri97@gmail.com>
2025-08-26 12:14:55 +0200

    firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -23,21 +23,4 @@ messaging.onBackgroundMessage((payload) => {
   self.registration.showNotification(title, options);
 });
 
-// Focus sull’app al tap della notifica
-self.addEventListener('notificationclick', function(event) {
-  event.notification.close();
-  const urlToOpen = new URL('/', self.location.origin).href;
 
-  event.waitUntil(
-    clients.matchAll({ type: 'window', includeUncontrolled: true }).then(windowClients => {
-      for (let client of windowClients) {
-        if (client.url === urlToOpen && 'focus' in client) {
-          return client.focus();
-        }
-      }
-      if (clients.openWindow) {
-        return clients.openWindow(urlToOpen);
-      }
-    })
-  );
-});
